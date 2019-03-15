8c55427032452dc049b6fc30abd9feade1fdf55a
Glitch (wapuu-bot) <none>
2019-03-15 14:19:25 +0000

    wcmia 2019 friday update

--- skills/wapuu-hears.js
+++ skills/wapuu-hears.js
@@ -14,57 +14,11 @@ var monk = require('monk');
 var db = monk(process.env.MONGO_URI);
 
 module.exports = function(controller) {
-/*
-    controller.hears(['^find (.*)'], ['direct_message','direct_mention','mention'], function(bot, message) {
-
-          var term = message.match[1];           //var termPattern = new RegExp('.*' + term + '.*', 'i');
-          var termPattern = ".*" + term + ".*";
-
-          var results = [];
-          db.get('sessions').find(
-            
-              { $or: [ 
-                { title: {$regex: termPattern, $options: 'i'} },
-                { speakers: {$regex: termPattern, $options: 'i'} } 
-                ]
-              },
-              {sort: {session_time: 1}}
-              
-          ).then((sessions) => {
-            
-             sessions.forEach(s => {
-               
-                 var track = s.track[0] ? s.track[0].name.replace('&amp;', '&').replace('&#8217;', "'") : '';
-               
-                 if (s.track[0] && s.track.length > 1) {
-                   track = 'All Tracks'
-                 }
-               
-                 var attachment = {
-                   title: s.title.replace('&amp;', '&').replace('&#8217;', "'"),
-                   author_name: s.speakers.join(', '),
-                   text: track,
-                   footer: moment.unix(s.session_time).format('ddd h:mm A')
-                 };
-                 results.push(attachment);
-
-             });
-            
-              bot.reply(message, {
-                  text: config.getRandom(greeting.deliveryWords) + '...',
-                  attachments: results
-              });
-
-          });     
-                
-      
-    });
-*/
-        // admin-announce 
+    // admin-announce 
     // allow an admin to send a message to main channel from a DM with the bot
     // message comes as if from the bot in the announceChannel
 
-    controller.hears(['admin-announce (.*)'], 'direct_message', function(bot, message) {
+    controller.hears(['say (.*)'], 'direct_message', function(bot, message) {
       /*
         if (config.adminUsers.indexOf(message.user) === -1) {
             return;
@@ -75,31 +29,32 @@ module.exports = function(controller) {
         // announceChannel is the channel to make announcement in, i.e., #general
         bot.say({channel: config.channelAnnounce, text: msg});
     });
-
-    controller.hears(['kids'], ['direct_message','direct_mention','mention','ambient'], function(bot, message) {
+  
+    controller.hears(['kids'], ['direct_message','direct_mention','mention'], function(bot, message) {
         bot.reply(message, responses.kidsReply());
         
     });
 
-    controller.hears(['directions', 'lost', 'map to f', 'map to c'], ['direct_message','direct_mention','mention','ambient'], function(bot, message) {
+    controller.hears(['parking', 'directions', 'lost', 'map to f', 'map to c'], ['direct_message','direct_mention','mention'], function(bot, message) {
         bot.reply(message, responses.helpDirections);
         
     });
 
-    controller.hears(['wifi', 'wi-fi'], ['direct_message','direct_mention','mention','ambient'], function(bot, message) {
+    controller.hears(['wifi', 'wi-fi'], ['direct_message','direct_mention','mention'], function(bot, message) {
         bot.reply(message, responses.wifiReply() );
         
     });
 
-    controller.hears(['slides', 'slideshare', 'slide link'], ['direct_message','direct_mention','mention','ambient'], function(bot, message) {
+    controller.hears(['slides', 'slideshare', 'slide link'], ['direct_message','direct_mention','mention'], function(bot, message) {
         bot.reply(message, responses.slidesReply() );
         
     });
 
-    controller.hears(['registration','register','check in'], ['direct_message','direct_mention','mention','ambient'], function(bot, message) {
+    controller.hears(['registration','register','check in'], ['direct_message','direct_mention','mention'], function(bot, message) {
         bot.reply(message, responses.registrationReply());
         
     });
+  
 /*
     controller.hears(['ticket','refund'], ['direct_message','direct_mention','mention','ambient'], function(bot, message) {
         bot.reply(message, responses.ticketsReply());
@@ -112,7 +67,7 @@ module.exports = function(controller) {
     });
 
     controller.hears(['help'], ['direct_message','direct_mention','mention'], function(bot, message) {
-        bot.reply(message, responses.helpUsage);
+        bot.reply(message, responses.helpSlash());
         
     });
 
@@ -121,16 +76,28 @@ module.exports = function(controller) {
         
     });
 
+    controller.hears(['friday'], ['direct_message','direct_mention','mention'], function(bot, message) {
+        bot.reply(message, responses.helpFriday);
+        
+    });
+
+    controller.hears(['saturday'], ['direct_message','direct_mention','mention'], function(bot, message) {
+        bot.reply(message, responses.helpSaturday);
+        
+    });
+
+    controller.hears(['sunday'], ['direct_message','direct_mention','mention'], function(bot, message) {
+        bot.reply(message, responses.helpSunday);
+        
+    });
+
     controller.hears(['schedule'], ['direct_message','direct_mention','mention'], function(bot, message) {
-        bot.reply(message, responses.helpSchedule);
-        
+        bot.reply(message, responses.helpSchedule);        
     });
-/*
-    controller.hears(['live', 'video'], ['direct_message','direct_mention','mention'], function(bot, message) {
-        bot.reply(message, responses.liveStreamReply);
-        
+
+    controller.hears(['video', 'watch live', 'stream'], ['direct_message','direct_mention','mention','ambient'], function(bot, message) {
+        bot.reply(message, responses.liveStreamReply());        
     });
-*/
   
     coffee.words.forEach(g => {
         controller.hears(g.hear, ['direct_message','direct_mention','mention','ambient'], function(bot, message) {
@@ -154,13 +121,15 @@ module.exports = function(controller) {
     greeting.words.forEach(g => {
         controller.hears(g.hear, ['direct_message','direct_mention','mention'], function(bot, message) {
             bot.reply(message, config.getRandom(g.reply) + ' ' + config.getRandomEmoji(greeting.emoji));
-              console.log(message);
         });
     });
 
     greeting.ambientWords.forEach(g => {
-        controller.hears(g.hear, ['ambient'], function(bot, message) {
-            bot.reply(message, config.getRandom(g.reply) + ' ' + config.getRandomEmoji(greeting.emoji));
+        controller.hears(g.hear, ['direct_message','direct_mention','mention','ambient'], function(bot, message) {
+            if ((message.type === 'ambient' && message.channel === config.channelCoffee) ||
+                 (message.type != 'ambient')) {
+              bot.reply(message, config.getRandom(g.reply) + ' ' + config.getRandomEmoji(greeting.emoji));
+            }
         });
     });
 
--- skills/wapuu-slash.js
+++ skills/wapuu-slash.js
@@ -15,10 +15,18 @@ module.exports = function(controller) {
           bot.replyPrivate(message, responses.helpFood);
         } else if (message.text === 'links') {
           bot.replyPrivate(message, responses.helpLinks);
+        } else if (message.text === 'friday') {
+          bot.replyPrivate(message, responses.helpFriday);
+        } else if (message.text === 'saturday') {
+          bot.replyPrivate(message, responses.helpSaturday);
+        } else if (message.text === 'sunday') {
+          bot.replyPrivate(message, responses.helpSunday);
         } else if (message.text === 'schedule') {
           bot.replyPrivate(message, responses.helpSchedule);
-        } else if (message.text === 'find') {
-
+        } else if (message.text === 'directions'
+                   || message.text === 'parking' 
+                   || message.text === 'map') {
+          bot.replyPrivate(message, responses.helpDirections);
         } else {
           bot.replyPrivate(message, responses.helpUsage);
         }              
